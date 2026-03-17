d730e306e52ee894d1af92281b7d69996e07a8a7
Markus Weinzettl <futuresystems1323@gmail.com>
2026-03-17 07:39:29 +0100

    search filter

--- controller/route_load.py
+++ controller/route_load.py
@@ -20,5 +20,10 @@ def run_local_route():
     for electronic in all_electronic:
         print(electronic)
 
+    seach_for_book_title = db.search_entities(Book, "p.name", "Potter")
+    seach_for_book_autor = db.search_entities(Book, "b.autor", "Tolkin")
+    search_for_electronic_brand = db.search_entities(Electronic, "e.brand", "Apple")
+
+    print(search_for_electronic_brand, seach_for_book_autor, seach_for_book_title)
 if __name__ == "__main__":
     run_local_route()
--- database/db_manager.py
+++ database/db_manager.py
@@ -1,5 +1,6 @@
 import psycopg2
 
+
 class DB_Manager:
     def __init__(self):
         self.params = {
@@ -10,14 +11,36 @@ class DB_Manager:
         }
 
     def save_entity(self, entity):
-        with psycopg2.connect(**self.params) as conn:
-            with conn.cursor() as cursor:
-                for query, data in entity.get_save_queries():
-                    cursor.execute(query, data)
+        try:
+            with psycopg2.connect(**self.params) as conn:
+                with conn.cursor() as cursor:
+                    for query, data in entity.get_save_queries():
+                        cursor.execute(query, data)
+                    conn.commit()
+        except psycopg2.Error as e:
+            print(f"Fehler beim Speichern: {e}!")
 
     def load_entities(self, entity_class):
-        with psycopg2.connect(**self.params) as conn:
-            with conn.cursor() as cursor:
-                cursor.execute(entity_class.get_load_query())
-                rows = cursor.fetchall()
-                return [entity_class(*row) for row in rows]
+        try:
+            with psycopg2.connect(**self.params) as conn:
+                with conn.cursor() as cursor:
+                    cursor.execute(entity_class.get_load_query())
+                    rows = cursor.fetchall()
+                    return [entity_class(*row) for row in rows]
+        except psycopg2.Error as e:
+            print(f"Fehler beim Laden: {e}!")
+
+    def search_entities(self, entity_class, column, search_term):
+        try:
+            with psycopg2.connect(**self.params) as conn:
+                with conn.cursor() as cursor:
+                    query = f"{entity_class.get_load_query()} WHERE {column} ILIKE %s"
+
+                    params = (f"%{search_term}%",)
+
+                    cursor.execute(query, params)
+                    rows = cursor.fetchall()
+                    return [entity_class(*row) for row in rows]
+        except psycopg2.Error as e:
+            print(f"Fehler bei der Suche: {e}!")
+            return []
